9ea41a0a1869e9d67eddb781f446683b855b36d3
aqys <141941584+aqys@users.noreply.github.com>
2024-05-20 18:46:58 +0200

    Simplified the code

--- script.py
+++ script.py
@@ -1,4 +1,2 @@
 with open("kontakter.txt", "a") as file:
-    questions = ["Fornavn", "Efternavn", "Email", "Telefon nr.", "Addresse"]
-    for q in questions:
-        file.write(f"{q}: {input(f'{q}: ')}\n")
+    file.writelines(f"{q}: {input(f'{q}: ')}\n" for q in ["Fornavn", "Efternavn", "Email", "Telefon nr.", "Addresse"])
